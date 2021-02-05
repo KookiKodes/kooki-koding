f187c3c75c470ada1f5b8adb00d7bd04339e0503
kookikodes <kookikodes@gmail.com>
2021-02-05 07:28:02 -0800

    minor form fixes

--- components/interfaces/Form.ts
+++ components/interfaces/Form.ts
@@ -1,8 +1,6 @@
 export interface FInput {
   type: string;
   label: string;
-  colSpan: string | number;
-  inputHeight: string | number;
   inputInfo: "input" | "textarea";
   name: string;
 }
--- components/subcomponents/FormInput.tsx
+++ components/subcomponents/FormInput.tsx
@@ -12,10 +12,15 @@ import { FInput } from "@components/interfaces/Form";
 export default function FormInput(props: FInput) {
   const { colors } = useThemedContext();
   const container = useAnimation();
-
-  function createInput() {
-    if (props.inputInfo === "input") {
-      return (
+  if (props.inputInfo === 'input') {
+  return (
+      <motion.div
+        variants={formVariants.inputContainer(colors)}
+        className="flex flex-col self-center w-full h-full xl:justify-end col-span-1"
+      >
+        <motion.label className="self-start mb-4 text-2xl" htmlFor={props.name}>
+          {props.label}
+        </motion.label>
         <motion.input
           name={props.name}
           animate={container}
@@ -25,11 +30,20 @@ export default function FormInput(props: FInput) {
           whileHover="hover"
           onFocus={() => container.start("focus")}
           onBlur={() => container.start("initial")}
-          className={`w-full p-4 text-xl lg:text-3xl border rounded-lg focus:outline-none h-8 lg:h-${props.inputHeight} focus:shadow-lg`}
+          className="w-full p-4 text-xl lg:text-3xl border rounded-lg focus:outline-none h-8 lg:h-14 focus:shadow-lg"
         />
-      );
-    } else {
-      return (
+      </motion.div>
+    )
+  }
+  else {
+    return (
+      <motion.div
+        variants={formVariants.inputContainer(colors)}
+        className="flex flex-col self-center w-full h-full xl:justify-end col-span-full"
+      >
+        <motion.label className="self-start mb-4 text-2xl" htmlFor={props.name}>
+          {props.label}
+        </motion.label>
         <motion.textarea
           name={props.name}
           animate={container}
@@ -38,21 +52,9 @@ export default function FormInput(props: FInput) {
           whileHover="hover"
           onFocus={() => container.start("focus")}
           onBlur={() => container.start("initial")}
-          className={`w-full resize-none text-xl p-4 lg:text-3xl border rounded-lg focus:outline-none h-30 lg:h-${props.inputHeight} focus:shadow-lg`}
+          className="w-full resize-none text-xl p-4 lg:text-3xl border rounded-lg focus:outline-none h-30 lg:h-40 focus:shadow-lg"
         />
-      );
+      </motion.div>
+    )
     }
-  }
-
-  return (
-    <motion.div
-      variants={formVariants.inputContainer(colors)}
-      className={`flex flex-col self-center w-full h-full xl:justify-end xl:col-span-${props.colSpan}`}
-    >
-      <motion.label className="self-start mb-4 text-2xl" htmlFor={props.name}>
-        {props.label}
-      </motion.label>
-      {createInput()}
-    </motion.div>
-  );
 }
