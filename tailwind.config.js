23aed63f801fa25f8dda298e39a11a3b772f9930
kookikodes <kookikodes@gmail.com>
2021-02-05 06:28:39 -0800

    "Minor window height fix"

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    minHeight: {
+      "px-600": "600px",
+    },
     extend: {},
   },
   variants: {
