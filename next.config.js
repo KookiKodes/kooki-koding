3b6f229b08c4568988d485d7a68a0a54597fa631
KookiKodes <kookikodes@gmail.com>
2021-09-01 23:40:13 -0700

    minor update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-const redis = require("redis");
-const { REDIS_PORT, REDIS_HOST, REDIS_PASSWORD } = process.env;
-
-const redis_client = redis.createClient(REDIS_PORT, REDIS_HOST);
-redis_client.auth(REDIS_PASSWORD);
-
-redis_client.on("connect", function(error) {
-  if (error) return console.log(error);
-  console.log("Redis Database Connected");
-});
-
 module.exports = {
   serverRuntimeConfig: {
     redis_client,
