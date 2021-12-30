19836863898b6d9c85a72d4ad27dc0cf3e54d7b2
KookiKodes <kookikodes@gmail.com>
2021-12-29 22:29:39 -0800

    added custom favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,37 @@
 import Document, {
-	Html,
-	Head,
-	Main,
-	NextScript,
-	DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
 } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "@static/themes";
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return initialProps;
-	}
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
 
-	render() {
-		return (
-			<Html>
-				<Head />
-				<body>
-					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            href="futuristic-circle-favicon.png"
+          />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
